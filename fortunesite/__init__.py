d39311ad51d108674da6528ee12380d89f3bb32b
Nessad Choudhury <nessadchoudhury@gmail.com>
2019-10-14 15:59:32 -0400

    adding forms and functionality for forms

--- fortunesite/__init__.py
+++ fortunesite/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8OG$FGd9j&QF0sfwwdIf'
 
 from fortunesite import routes
--- fortunesite/routes.py
+++ fortunesite/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from fortunesite import app
-from forms import SubmitForm
+from fortunesite.forms import SubmitForm
 
 fortune = "You will live a long and healthy life"
 
@@ -16,8 +16,8 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/submit', methods=('GET', 'POST'))
+@app.route('/submit', methods=['GET', 'POST'])
 def submit():
-    form = SubmitForm
-    if form.validate_on_submit():
-        return 
+    form = SubmitForm()
+    return render_template('create_fortune.html', title='New Fortune',
+                            form=form)
