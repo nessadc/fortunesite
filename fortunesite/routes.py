d0e2a9044cdee4446118ecab7867cddccfd27e5f
Nessad Choudhury <nessadchoudhury@gmail.com>
2019-10-14 15:27:11 -0400

    starting to add basic POST functionality

--- fortunesite/routes.py
+++ fortunesite/routes.py
@@ -1,6 +1,6 @@
-# my pages are defined here
 from flask import render_template
 from fortunesite import app
+from forms import SubmitForm
 
 fortune = "You will live a long and healthy life"
 
@@ -14,3 +14,10 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+
+@app.route('/submit', methods=('GET', 'POST'))
+def submit():
+    form = SubmitForm
+    if form.validate_on_submit():
+        return 
