f0319f0742e04c6ffe352c379925535016a2dc54
Nessad Choudhury <nessadchoudhury@gmail.com>
2019-10-18 01:42:19 -0400

    wrapping up initial registration and logging in of users

--- fortunesite/routes.py
+++ fortunesite/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, flash, redirect, url_for
-from fortunesite import app
-from fortunesite.forms import LoginForm, SubmitForm
+from flask import render_template, flash, redirect, url_for, request
+from fortunesite import app, db
+from fortunesite.forms import LoginForm, SubmitForm, RegistrationForm
 from fortunesite.models import User
 from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 
 fortune = "You will live a long and healthy life"
@@ -19,6 +20,20 @@ def about():
     return render_template("about.html")
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -30,7 +45,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Log In', form=form)
 
 
