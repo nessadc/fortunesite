c2174c2740f0e80dbe8beeef563f07e3ffa70e04
Nessad Choudhury <nessadchoudhury@gmail.com>
2019-10-26 09:41:41 +0200

    followers function, and new database (removed nullable false functions)

--- fortunesite/models.py
+++ fortunesite/models.py
@@ -6,15 +6,26 @@ from fortunesite import login
 from hashlib import md5
 
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True,
                          nullable=False)
-    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
     fortunes = db.relationship('Fortune', backref='author', lazy=True)
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -30,13 +41,32 @@ class User(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Fortune.query.join(
+            followers, (followers.c.followed_id == Fortune.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Fortune.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Fortune.timestamp.desc())
+
 
 class Fortune(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, nullable=False,
+    timestamp = db.Column(db.DateTime, index=True,
                           default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Fortune {}>'.format(self.content)
