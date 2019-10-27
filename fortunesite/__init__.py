20d4edf31b4bf90529e3860e71d4e6a013862b38
Nessad Choudhury <nessadchoudhury@gmail.com>
2019-10-27 15:59:53 -0400

    modifying look of most pages, no app logic changes

--- fortunesite/__init__.py
+++ fortunesite/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 sql_uri = '/Users/nessad/Programming/fortunesite/fortunesite/site.db'
 
@@ -53,5 +54,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
+bootstrap = Bootstrap(app)
 
 from fortunesite import routes, models, errors
