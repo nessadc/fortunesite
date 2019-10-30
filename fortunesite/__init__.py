1da4b7b21a833d16cf16a0a57d7a58d7c2b8bf41
Nessad Choudhury <nessadchoudhury@gmail.com>
2019-10-29 20:18:42 -0400

    adding better time handling using moment.js

--- fortunesite/__init__.py
+++ fortunesite/__init__.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 sql_uri = '/Users/nessad/Programming/fortunesite/fortunesite/site.db'
 
@@ -56,5 +57,6 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from fortunesite import routes, models, errors
