37decabef642ae7f83cc8e6ba8c4f6492521408e
Nessad Choudhury <nessadchoudhury@gmail.com>
2019-11-03 14:48:57 -0500

    another init fix

--- fortunesite/__init__.py
+++ fortunesite/__init__.py
@@ -25,8 +25,8 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    db.init_app(app, db)
-    migrate.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
     bootstrap.init_app(app)
